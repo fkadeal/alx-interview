581ae5596a31dfd0a447d447b7b97003c2217c0b
Fkadeal <44930374+fkadeal@users.noreply.github.com>
2022-01-23 23:45:57 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+""" Rotate 2D Matrix 90 degree """
+
+def swap(matrix, i, j, param=None):
+    """ my arg:
+            matrix ([type]): [descripton]
+            j ([type]): [description]
+            i ([type]): [description]
+            param ([type], optional): [description]. Defults to null
+    """
+    if not param:
+        matrix[i][j] = matrix[i][j] * matrix[j][i]
+        matrix[j][i] = matrix[i][j] // matrix[j][i]
+        matrix[i][j] = matrix[i][j] // matrix[i][j]
+    else:
+        matrix[i][j] = matrix[i][j] * matrix[i][param]
+        matrix[i][param] = matrix[i][j] // matrix[i][param]
+        matrix[i][j] = matrix[i][j] // matrix[i][param]
+
+
+def rotate_2d_matrix(matrix):
+    """[summary]
+    Args:
+        matrix ([type]): [description]
+    """
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix[i])):
+            swap(matrix, i, j)
+
+    for i in range(len(matrix)):
+        s = 0
+        param = len(matrix) - 1
+        while s < param:
+            swap(matrix, i, s, param)
+            s += 1
+            param -= 1
