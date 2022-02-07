ad793251479c7a0a986845d22df6b1a7d5a143fb
Fkadeal <44930374+fkadeal@users.noreply.github.com>
2022-02-07 05:35:48 +0300

    PEP8 validation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
 
-
 def island_perimeter(grid):
     """  returns the perimeter of the island described in grid """
 
